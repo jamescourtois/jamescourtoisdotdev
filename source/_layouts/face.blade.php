b08cee5b71a0098d2046543c4f7137a4b1c8982f
jamescourtois <james.r.courtois@gmail.com>
2026-06-12 13:33:49 -0500

    Uncanny valley crossing

--- source/_layouts/face.blade.php
+++ source/_layouts/face.blade.php
@@ -1,4 +1,4 @@
-<div class="jamesface {{ $class ?? '' }}" title="I drew and animated my face using html and css.">
+<div class="face {{ $class ?? '' }}" title="I drew and animated my face using html and css.">
 	<a href="/" class="absolute inset-0 z-[999]"><span class="sr-only">Go Home</span></a>
     <div class="hair">
         <div class="top">
@@ -11,41 +11,31 @@
     </div>
 
     <div class="head"></div>
-
-		<div class="ear left">
-    </div>
-
-    <div class="ear right">
-    </div>
-
+		<div class="ear left"></div>
+    <div class="ear right"></div>
     <div class="eyebrow left"></div>
-
+		<div class="eyebrow right"></div>
     <div class="eye left">
-        <div class="pupil"></div>
-        <div class="iris"></div>
+			<div class="iris">
+					<div class="pupil"></div>
+				</div>
     </div>
-
-    <div class="eyebrow right"></div>
-
     <div class="eye right">
-        <div class="pupil"></div>
-        <div class="iris"></div>
+			<div class="iris">
+					<div class="pupil"></div>
+				</div>
     </div>
-
     <div class="nose">
         <div class="bridge"></div>
         <div class="bridge-outer"></div>
         <div class="nostrils">
         </div>
     </div>
-
     <div class="mouth">
         <div class="top-lip">
             <span class="fill"></span>
         </div>
-        <div class="mouth-inner">
-            <div class="teeth"></div>
-        </div>
+        <div class="teeth"></div>
         <div class="bottom-lip">
             <span class="fill"></span>
         </div>
